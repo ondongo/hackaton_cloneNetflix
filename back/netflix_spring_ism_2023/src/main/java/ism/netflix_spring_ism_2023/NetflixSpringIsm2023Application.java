915a220e36ba516d39c2f10041e4dbaa597ee44e
Mamadou Barry <barrymamadou.6252@gmail.com>
2023-05-27 11:08:23 +0000

    premier depot

---
+++ back/netflix_spring_ism_2023/src/main/java/ism/netflix_spring_ism_2023/NetflixSpringIsm2023Application.java
@@ -0,0 +1,13 @@
+package ism.netflix_spring_ism_2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixSpringIsm2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NetflixSpringIsm2023Application.class, args);
+    }
+
+}
---
+++ back/netflix_spring_ism_2023/src/test/java/ism/netflix_spring_ism_2023/NetflixSpringIsm2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ism.netflix_spring_ism_2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixSpringIsm2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
